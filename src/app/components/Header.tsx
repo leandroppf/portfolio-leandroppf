cbd39a7cf0e062cf498a48882e82bc2bb8b041d1
Leandro Pedroso <leandroppf@gmail.com>
2025-02-09 15:48:29 -0300

    feat: updated HTML project structure

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,18 +1,23 @@
-'use client'
-
 import Image from 'next/image'
 
 export default function Header() {
   return (
-    <header className="flex h-16 w-full flex-row items-center px-8">
+    <header className="flex w-full flex-col items-start py-16 xl:sticky xl:top-0 xl:h-screen xl:max-w-[calc(1280px_*_0.4)]">
       <Image
         src="/brand.svg"
         alt="Portifolio brand"
-        className="fixed"
+        className="mb-8"
         width={40}
         height={40}
         priority
       />
+
+      <h1 className="mb-4 text-secondary">Hi, my name is</h1>
+      <h2 className="mb-8 text-4xl font-bold">Leandro Pedroso.</h2>
+      <h3 className="text-secondary">
+        I’m a passionate Software Engineer from Brazil, specializing in building
+        accessible and flawless digital experiences.
+      </h3>
     </header>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,10 @@ import Header from '@/app/components/Header'
 
 export default function Home() {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-12">
+    <div className="grid w-full max-w-7xl justify-start gap-8 px-12 xl:grid-cols-[4fr_6fr]">
       <Header />
-      <main className="grid w-full max-w-7xl grid-rows-1 justify-start gap-8 px-12 xl:grid-cols-[4fr_6fr]">
-        <section className="xl:fixed xl:max-w-[calc(1280px_*_0.4)]">
-          <h1 className="mb-4 text-secondary">Hi, my name is</h1>
-          <h2 className="mb-8 text-4xl font-bold">Leandro Pedroso.</h2>
-          <h3 className="text-secondary">
-            I’m a passionate Software Engineer from Brazil, specializing in
-            building accessible and flawless digital experiences.
-          </h3>
-        </section>
-
-        <section className="xl:col-start-2">
+      <main className="xl:col-start-2">
+        <section>
           <Experience />
         </section>
 
