968ec0e301a8329848641ae49da64c0100d0a0d0
Leandro Pedroso <leandroppf@gmail.com>
2025-02-09 01:18:30 -0300

    fix: Experience data fetching. Doesn't make sense to fetch my own server on server side

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,19 +1,19 @@
+import ExperienceData from '@/data/experience.json'
 import type { Experience } from '@/models/experience'
 import ExperienceCard from './ExperienceCard'
 
-export default async function Experience() {
-  const data = await fetch('http://localhost:3000/api/experience', {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  const { experiences } = (await data.json()) as { experiences: Experience[] }
+export default function Experience() {
+  const experiences = ExperienceData as Experience[]
 
   return (
     <div className="flex w-full flex-col items-center justify-center gap-8">
-      {experiences.map((experience) => (
-        <ExperienceCard key={experience.company.name} experience={experience} />
-      ))}
+      {experiences.length &&
+        experiences.map((experience) => (
+          <ExperienceCard
+            key={experience.company.name}
+            experience={experience}
+          />
+        ))}
     </div>
   )
 }
