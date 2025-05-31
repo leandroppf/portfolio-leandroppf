244c280808c48fd03a07add8ab0efff9e46be448
Leandro Pedroso <leandroppf@gmail.com>
2025-05-31 16:37:40 +0100

    feat: Projects section with data and ProjectCard component

---
+++ src/app/components/ProjectCard.tsx
@@ -0,0 +1,65 @@
+import { ArrowUpRight, GithubLogo } from '@phosphor-icons/react/dist/ssr'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Project } from '@/models/project'
+import { cn } from '@/utils/cn'
+import Skills from './Skills'
+
+interface ProjectCardProps {
+  project: Project
+}
+
+export default function ProjectCard({ project }: ProjectCardProps) {
+  return (
+    <div
+      className={cn(
+        'flex w-full flex-row gap-4 overflow-hidden rounded-md border border-sky-900/10 bg-sky-900/10 shadow shadow-sky-900/10',
+        'dark:border-sky-900/50 dark:shadow-sky-900/50',
+      )}
+    >
+      <Image
+        src={project.imageURL}
+        alt={project.title}
+        width={300}
+        height={200}
+        className="h-52 w-auto"
+      />
+
+      <div className={cn('flex flex-col gap-1 p-2 text-secondary')}>
+        <span className="font-bold">{project.title}</span>
+        <p
+          className="text-sm"
+          dangerouslySetInnerHTML={{ __html: project.description }}
+        />
+        <Skills skills={project.technologies} />
+
+        <div className="mt-auto flex flex-row items-center gap-4">
+          <Link
+            href={project.githubURL}
+            aria-label={[project.title, 'Github URL'].join(' ')}
+            target="_blank"
+          >
+            <GithubLogo
+              size={32}
+              alt="Github Link"
+              className="rounded p-1 hover:bg-foreground active:opacity-90"
+            />
+          </Link>
+          <Link
+            href={project.liveURL}
+            aria-label={[project.title, 'Live URL'].join(' ')}
+            target="_blank"
+            className="group inline-flex h-10 items-center gap-2 text-sm text-secondary underline underline-offset-2"
+          >
+            View live
+            <ArrowUpRight
+              size={14}
+              className="invisible -mt-2 text-primary transition-all duration-300 group-hover:visible group-hover:-mt-4 group-hover:ml-2"
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,5 +1,16 @@
+import ProjectData from '@/data/project.json'
+import type { Project } from '@/models/project'
+import ProjectCard from './ProjectCard'
+
 export default function Projects() {
+  const projects = ProjectData as Project[]
+
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-8"></div>
+    <div className="flex w-full flex-col items-center justify-center gap-8">
+      {projects.length &&
+        projects.map((project) => (
+          <ProjectCard key={project.title} project={project} />
+        ))}
+    </div>
   )
 }
